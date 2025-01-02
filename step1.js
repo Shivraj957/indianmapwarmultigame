bffefa8d18d58fd2131a215ac1bd5e4f9d22f68f
Shivraj Goswami <shivrajgoswami957@gmail.com>
2025-01-02 12:42:44 +0530

    Create step1.js

---
+++ step1.js
@@ -0,0 +1,14 @@
+document.getElementById(Form1).addEventListener("submit", function(event) {
+  const Username = document.getElementById("Username");
+  const Region = document.getElementById("Region");
+  if (!username.value.trim()) {
+    alert("Username is required!");
+    event.preventDefault();
+    return;
+            }
+  if (region.value === "0") 
+    alert("Please select a region!");
+    event.preventDefault(); // Prevent form submission
+    return;
+            }
+});
